5411878a84ddff446aa07f29e34a6e3a15e18276
Temp <nirshah0209@gmail.com>
2026-03-05 19:16:47 +0530

    fix: remove unused tf variables, restore TF_VAR_s3_bucket in pipeline

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,18 +9,5 @@ variable "instance_type" {
 }
 
 variable "key_name" {
-  description = "Name of the EC2 key pair for SSH access (set via TF_VAR_key_name)"
-}
-
-variable "s3_bucket" {
-  description = "Name of the S3 bucket used to store the Terraform remote state (set via TF_VAR_s3_bucket)"
-}
-
-variable "github_owner" {
-  description = "GitHub username / org that owns the repository"
-}
-
-variable "github_repo" {
-  description = "GitHub repository name (e.g. mini-calc)"
-  default     = "mini-calc"
+  description = "Name of the EC2 key pair for SSH access (set via TF_VAR_key_name or GitHub secret)"
 }
